4eb2e027667f03ba782f843e675730696c7ec50b
NikitaErmakov <ermafin@gmail.com>
2025-03-02 21:30:21 +0300

    reg and log endpoints done

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -4,14 +4,13 @@ from sqlalchemy.future import select
 
 from app.database import get_db_session
 from app.models.user import User
-from app.schemas.user import UserRegistration, UserRegistrationResponse
+from app.schemas.user import UserRegistration, UserRegistrationResponse, UserLogin, UserLoginResponse
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
 
 
 @router.post("/register", response_model=UserRegistrationResponse, status_code=status.HTTP_201_CREATED)
 async def registrate_user(user_data: UserRegistration, db: AsyncSession = Depends(get_db_session)):
-    print(user_data)
     existing_user = await db.execute(select(User).where(User.email == user_data.email))
     if existing_user.scalar_one_or_none():
         raise HTTPException(
@@ -37,3 +36,27 @@ async def registrate_user(user_data: UserRegistration, db: AsyncSession = Depend
     await db.refresh(new_user)
 
     return UserRegistrationResponse(result=True)
+
+
+@router.post("/login", response_model=UserLoginResponse, status_code=status.HTTP_200_OK)
+async def login_user(user_data: UserLogin, db: AsyncSession = Depends(get_db_session)):
+    user = await db.execute(
+        select(User).where(
+            (User.email == user_data.email_or_login) | (User.login == user_data.email_or_login)
+        )
+    )
+    user = user.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный email/логин или пароль",
+        )
+
+    if not user.check_password(user_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный email/логин или пароль",
+        )
+
+    return UserLoginResponse(result=True)
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,5 +6,16 @@ class UserRegistration(BaseModel):
     login: str
     password: str | int
 
+
+class UserLogin(BaseModel):
+    email_or_login: str
+    password : str
+
+
+class UserLoginResponse(BaseModel):
+    result: bool
+
+
 class UserRegistrationResponse(BaseModel):
     result: bool
+
